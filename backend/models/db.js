4ae1bf65ea8f6fc591726dcd69e44305a5b75851
Jadest03 <jadest1018@gmail.com>
2023-09-11 23:05:56 +0900

    backend

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,17 +1,18 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+  host: "svc.sel5.cloudtype.app:32595",
+  user: "jadest03",
   password: "Wjsalsrb0725!", //비번
-  database: "main_db", //db명
+  database: "Todo", //db명
 });
 
 connection.connect((error) => {
   if (error) {
     console.log(error);
+  } else {
+    console.log("데이터베이스 연결됨.");
   }
-  console.log("데이터베이스 연결됨.");
 });
 
 module.exports = connection;
