9d180845e7f765f0834807bb1157a3a854e6b038
YoungHun <younghun4218@gmail.com>
2023-09-16 17:01:34 +0900

    캘린더,todo, 달성률 페이지 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "./Register";
-import Login from "./Login";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './Register';
+import Login from './Login';
+import Layout from './Layout';
+import TodoPage from './TodoPage';
+import CalendarPage from './CalendarPage';
+import AchievementPage from './AchievementPage';
 
 function App() {
-  return (
-    <main className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Router>
-    </main>
-  );
+	return (
+		<main className='App'>
+			<Router>
+				<Routes>
+					<Route path='/' element={<Layout />}>
+						<Route path='todo' element={<TodoPage />} />
+						<Route path='calendar' element={<CalendarPage />} />
+						<Route
+							path='achievement'
+							element={<AchievementPage />}
+						/>
+					</Route>
+
+					<Route path='/' element={<Login />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/register' element={<Register />} />
+				</Routes>
+			</Router>
+		</main>
+	);
 }
 
 export default App;
