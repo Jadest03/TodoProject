2ec099d04ef6a4fe147215439418bc85cf04d957
YoungHun <younghun4218@gmail.com>
2023-09-16 17:03:06 +0900

    sidebar 페이지 생성

---
+++ src/Sidebar.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import TaskIcon from '@mui/icons-material/Task';
+
+const Sidebar = () => {
+	const navigate = useNavigate();
+	return (
+		<Box
+			sx={{
+				width: '100%',
+				maxWidth: 360,
+				bgcolor: 'background.paper',
+				height: '100%',
+			}}
+		>
+			<nav aria-label='sidemenu'>
+				<List>
+					<ListItem disablePadding>
+						<ListItemButton
+							onClick={() => {
+								navigate('/todo');
+							}}
+						>
+							<ListItemIcon>
+								<FormatListBulletedIcon />
+							</ListItemIcon>
+							<ListItemText primary='남은 할일' />
+						</ListItemButton>
+					</ListItem>
+					<ListItem disablePadding>
+						<ListItemButton
+							onClick={() => {
+								navigate('/calendar');
+							}}
+						>
+							<ListItemIcon>
+								<CalendarMonthIcon />
+							</ListItemIcon>
+							<ListItemText primary='캘린더' />
+						</ListItemButton>
+					</ListItem>
+					<ListItem disablePadding>
+						<ListItemButton
+							onClick={() => {
+								navigate('/achievement');
+							}}
+						>
+							<ListItemIcon>
+								<TaskIcon />
+							</ListItemIcon>
+							<ListItemText primary='달성률' />
+						</ListItemButton>
+					</ListItem>
+				</List>
+			</nav>
+		</Box>
+	);
+};
+
+export default Sidebar;
