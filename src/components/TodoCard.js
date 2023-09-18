fb19800c746035568dae7a2b55f6db9bbcc202a8
YoungHun <younghun4218@gmail.com>
2023-09-18 18:11:31 +0900

    투두 내용 수정 창 제작

---
+++ src/components/TodoCard.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import EditModal from './editModal';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function TodoCard(props) {
+	const openEditModal = () => {};
+
+	return (
+		<Card>
+			<CardContent>
+				<Typography
+					sx={{ fontSize: 14 }}
+					color='text.secondary'
+					gutterBottom
+				>
+					{props.remaintime} 남음
+				</Typography>
+				<Typography variant='h5' component='div'>
+					{props.title}
+				</Typography>
+				<Typography sx={{ mb: 1.5 }} color='text.secondary'>
+					{props.deadline}
+				</Typography>
+			</CardContent>
+			<CardActions>
+				<Button
+					variant='outlined'
+					size='small'
+					onClick={() => {
+						openEditModal();
+					}}
+				>
+					수정
+				</Button>
+				<Button variant='contained' size='small'>
+					완료
+				</Button>
+			</CardActions>
+		</Card>
+	);
+}
