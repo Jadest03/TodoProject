d1fdb8dcd7577bae4a7cec2897776b7f2d31606e
wgdcwd <honghcy01@gmail.com>
2023-09-18 22:45:16 +0900

    캘린더 페이지 수정
    투두카드 추가, 기본적인 css

--- src/AchievementPage.js
+++ src/AchievementPage.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const AchievementPage = () => {
-	return <div>AchievementPage</div>;
+  return <div>AchievementPage</div>;
 };
 
 export default AchievementPage;
