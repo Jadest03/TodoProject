77e12a7e7e9c8eda234eaab303ee89ff3a1144a6
YoungHun <younghun4218@gmail.com>
2023-09-18 17:35:30 +0900

    사이드 바를 반응형으로 수정

--- src/Layout.js
+++ src/Layout.js
@@ -1,19 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import Sidebar from './Sidebar';
+
+import ResponsiveDrawer from './Drawer';
 const Layout = () => {
 	return (
 		<div>
-			<header
-				style={{
-					background: 'skyblue',
-					padding: 16,
-					fontSize: 24,
-				}}
-			>
-				Core To Do List
-			</header>
 			<main style={{ display: 'flex', flexDirection: 'row' }}>
-				<Sidebar />
+				<ResponsiveDrawer />
 				<Outlet />
 			</main>
 		</div>
